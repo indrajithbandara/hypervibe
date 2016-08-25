d314d8fcabde5cfc01128706d654390feb14b21f
Geem7n <gadebesin@gmail.com>
2016-08-25 11:56:09 +0100

    chi square

--- src/cml/core/statistics/test.clj
+++ src/cml/core/statistics/test.clj
@@ -37,3 +37,55 @@
                               (/ (+ (count population-one) (count population-two)) 2)))))
 
 
+
+;http://commons.apache.org/proper/commons-math/apidocs/org/apache/commons/math3/stat/inference/ChiSquareTest.html
+
+
+(def observed [[60 300] [10 390]])
+
+
+(def total (double (esum observed)))
+(def row-total (map #(reduce + %) observed))
+(def column-total (map #(reduce + %) (columns observed)))
+
+(mmul [row-total column-total])
+
+(def expected-val '(33.1578947368421 326.8421052631579 36.8421052631579 363.1578947368421))
+
+(def expected
+  (for [rt (map #(reduce + %) observed)
+        ct (map #(reduce + %) (columns observed))]
+    (/ (* rt ct) (double (esum observed)))))
+
+(esum (emap op// (emap (fn [x] (op/* x x)) (sub observed expected))
+            expected))
+
+
+(pearson-chi-square (Independance. 60 33.16))
+
+;TODO seperate out observed vs expected values and use below (values) of (fn [observed expected] ...)
+(defn chi-square
+  "Assumes data to be in the form
+  [[x1 observed, x1 expected] [x2 observed, x2 expected]].
+   The Chi-square test computes the sum of the squares of the differences in values"
+  [values]
+  (reduce + 0
+          (map
+            (fn [[observed expected]]
+              (double
+                (/ (pow (- observed expected) 2)
+                   expected)))
+            values)))
+
+
+(for [rt (map #(reduce + %) observed)
+      ct (map #(reduce + %) (columns observed))]
+  [(first (first observed)) (/ (* rt ct) (double (esum observed)))])
+
+(for [rt (map #(reduce + %) observed)
+      ct (map #(reduce + %) (columns observed))
+      :let [out (atom [])
+            cnt (atom (count (first observed)))]
+      :when (not= @cnt (count @out))]
+  (swap! out conj
+         (/ (* rt ct) (double (esum observed)))))
