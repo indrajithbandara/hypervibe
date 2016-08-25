bdb5527bff3ebb6c5a9f0336e8d97d137949c873
Geem7n <gadebesin@gmail.com>
2016-08-25 20:47:07 +0100

    chi square

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [criterium "0.4.4"]
                  [com.google.guava/guava "19.0"]
-                 [net.mikera/core.matrix "0.51.0"]])
+                 [net.mikera/core.matrix "0.51.0"]
+                 [com.rpl/specter "0.12.0"]])
--- test/cml/core-test.clj
+++ test/cml/core-test.clj
@@ -6,8 +6,11 @@
             [cml.utils :refer [zip]]
             [cml.core.statistics.estimate :refer [one-sample-conf-inter two-sample-conf-inter]]
             [cml.core.statistics.test :refer [one-sample-ttest equal-var-ttest welch-ttest rep-measure-ttest]]
-            [cml.core.statistics.critical-value :refer [one-tail-cv two-tail-cv]]))
-
+            [cml.core.statistics.critical-value :refer [one-tail-cv two-tail-cv]]
+            [clojure.core.matrix.operators :as op]
+            [clojure.core.matrix :as matrix]
+            [clojure.walk :as walk]))
+(use 'com.rpl.specter)
 
 (deftest one-sample-t-test-test
   (is (= (one-sample-ttest {:sample population-one :h-mean 400})
@@ -115,3 +118,59 @@
     (confidence-interval (TwoSample. @(future (map mean sample))
                                      @(future (map #(:variance (variance (Sample. (mean %) %))) sample))
                                      @(future (map count sample)) critical-value)))
+;http://commons.apache.org/proper/commons-math/apidocs/org/apache/commons/math3/stat/inference/ChiSquareTest.html
+
+
+(def observed [[60 300] [10 390]])
+(def total (double (matrix/esum observed)))
+(def row-total (map #(reduce + %) observed))
+(def column-total (map #(reduce + %) (matrix/columns observed)))
+
+(def expected-val '(33.1578947368421 326.8421052631579 36.8421052631579 363.1578947368421))
+
+(def expected
+  (partition 2
+             (for [rt (map #(reduce + %) observed)
+                   ct (map #(reduce + %) (matrix/columns observed))]
+               (/ (* rt ct) (double (matrix/esum observed))))))
+
+(matrix/esum (matrix/emap op// (matrix/emap (fn [x] (op/* x x)) (matrix/sub observed expected))
+                          expected))
+
+
+;TODO seperate out observed vs expected values and use below (values) of (fn [observed expected] ...)
+(defn chi-square
+  "Assumes data to be in the form
+  [[x1 observed, x1 expected] [x2 observed, x2 expected]].
+   The Chi-square test computes the sum of the squares of the differences in values"
+  [values]
+  (reduce + 0
+          (map (fn [[observed expected]]
+                 (double (/ (matrix/pow (- observed expected) 2) expected))) values)))
+
+(let [observed (atom [2 3 4 5])]
+  (walk/walk (fn [vecs] (map (fn [nums] (do (* (first @observed) nums) (swap! observed next))) vecs)) vec [[1 2] [3 34]]))
+
+(let [exp (atom '(0 33.1578947368421 326.8421052631579 36.8421052631579 363.1578947368421))]
+  (matrix/emap (fn [x] (do (swap! exp pop) (* x (first @exp)))) observed))
+
+
+(matrix/esum
+  (let [exp (atom '(0 33.1578947368421 326.8421052631579 36.8421052631579 363.1578947368421))]
+    (matrix/emap
+      (fn [nums]
+        (do (swap! exp pop) (/ (* (- nums (first @exp)) (- nums (first @exp))) (first @exp)))) observed)))
+
+
+(defn chi-square [observed]
+  (let [expected (atom (conj (for [row-total (map #(reduce + %) observed)
+                                   column-total (map #(reduce + %) (matrix/columns observed))]
+                               (/ (* row-total column-total)
+                                  (double (matrix/esum observed)))) :sentinal))]
+    (matrix/esum (matrix/emap (fn [nums]
+                                (do (swap! expected next)
+                                    (/ (* (- nums (first @expected))
+                                          (- nums (first @expected)))
+                                       (first @expected)))) observed))))
+
+
