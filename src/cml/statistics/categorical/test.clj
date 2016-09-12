f0dce0f4da3e38e26775bef16574c0a7a770b1fa
adebesin <gregory.adebesin@gmail.com>
2016-09-12 21:15:31 +0100

    chi square performance

--- src/cml/statistics/categorical/test.clj
+++ src/cml/statistics/categorical/test.clj
@@ -37,20 +37,6 @@
 
 ;(neanderthal/mv! (fmap neanderthal/sum (neanderthal-native/dge 2 2 [1 2 3 4]) [1.0 1.0] [0 0]))
 
-(def a (range 100000))
-
-(def mtrx (neanderthal-native/dge 2 2 observed-vals))
-
-
-(for [x (neanderthal/mv! mtrx (neanderthal-native/dv [1 1]) (neanderthal-native/dv [0 0]))]
-  (println x))
-
-;;row
-(neanderthal/mv! (neanderthal/trans mtrx) (neanderthal-native/dv [1 1]) (neanderthal-native/dv [0 0]))
-
-;;cols
-(neanderthal/mv! mtrx (neanderthal-native/dv [1 1]) (neanderthal-native/dv [0 0]))
-
 (defrecord -Independance [observed nrows ncols]
   Categorical
   (pearson-chi-square [type]
@@ -58,43 +44,16 @@
           one-v (neanderthal/entry! (neanderthal-native/dv nrows) 1.0)
           zero-v (neanderthal-native/dv ncols)
           transpose (neanderthal/trans mtrx)
-          expected (for [row-total (neanderthal/mv! transpose one-v zero-v)
-                         column-total (neanderthal/mv! mtrx one-v zero-v)]
-                     (/ (* row-total column-total)          ;TODO outer product§
-                        (neanderthal/sum (neanderthal-native/dv observed))))] ;TODO in process of getting rid of for loop
+          sum-obs (neanderthal/sum (neanderthal-native/dv observed-vals))
+          row-total (neanderthal/mv! transpose one-v zero-v)
+          column-total (neanderthal/mv! mtrx one-v zero-v)] ;TODO in process of getting rid of for loop
       (assoc type :chi (neanderthal/sum (neanderthal/mv!
-                                          (fmap (fn ^double [^double x ^double y]
+                                          (fmap! (fn ^double [^double x ^double y]
                                                   (/ (* (- x y)
                                                         (- x y)) y))
                                                 transpose
-                                                (neanderthal-native/dge nrows ncols expected))
+                                                (fmap! (fn ^double [^double x] (/ x sum-obs))
+                                                       (neanderthal/rank
+                                                         row-total
+                                                         column-total)))
                                           one-v zero-v))))))
-
-
-(def row-total (neanderthal/mv! (neanderthal/trans mtrx)
-                                (neanderthal-native/dv [1 1])
-                                (neanderthal-native/dv [0 0])))
-
-(def column-total (neanderthal/mv! mtrx (neanderthal-native/dv [1 1])
-                                   (neanderthal-native/dv [0 0])))
-(def sum-obvs (neanderthal/sum (neanderthal-native/dv observed-vals)))
-
-(def rt [360 400])
-(def ct [70  690])
-
-
-
-(def rt-times-ct '(25200.0 248400.0 28000.0 276000.0))
-
-;rt =>  #RealBlockVector[double, n:2, stride:1](360.0 400.0)
-;ct =>  #RealBlockVector[double, n:2, stride:1](70.0 690.0)
-
-
-(def foo (neanderthal-native/dge 2 2 [60 300 10 390]))
-
-
-(neanderthal/rank
-  column-total
-  row-total)
-
-
