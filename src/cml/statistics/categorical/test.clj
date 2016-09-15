8b448060a0e84b753f6d3dbf28da3f5341b6d6df
adebesin <gregory.adebesin@gmail.com>
2016-09-15 21:11:38 +0100

    out of memory fix

--- src/cml/statistics/categorical/test.clj
+++ src/cml/statistics/categorical/test.clj
@@ -1,6 +1,7 @@
 (ns cml.statistics.categorical.test
   (:require [uncomplicate.neanderthal.native :as neanderthal-native]
-            [uncomplicate.neanderthal.core :as neanderthal])
+            [uncomplicate.neanderthal.core :as neanderthal]
+            [uncomplicate.commons.core :refer [with-release]])
   (:use [uncomplicate.fluokitten core jvm]))
 (use 'criterium.core)
 
@@ -8,24 +9,22 @@
   (pearson-chi-square [s] "Conducts a Chi Square test on a categorical data set"))
 
 
-;TODO see if fmap! has something to do with out of memory and change to fmap if so
-;TODO increase GC heap space in leiningen
-
 (defrecord Independance [observed nrows ncols]
   Categorical
   (pearson-chi-square [type]
-    (let [mtrx (neanderthal-native/dge nrows ncols observed)
-          one-v (neanderthal/entry! (neanderthal-native/dv nrows) 1.0)
-          zero-v (neanderthal-native/dv ncols)
-          sum-obs (neanderthal/sum (neanderthal-native/dv observed))
-          transposed-mtrx (neanderthal/trans mtrx)]
-      (assoc :chi (neanderthal/sum (neanderthal/mv!
+    (with-release [mtrx (neanderthal-native/dge nrows ncols observed)
+                   one-v (neanderthal/entry! (neanderthal-native/dv nrows) 1.0)
+                   zero-v (neanderthal-native/dv ncols)
+                   transposed-mtrx (neanderthal/trans mtrx)]
+      (assoc type :chi (neanderthal/sum (neanderthal/mv!
                                           (fmap! (^:once fn* ^double [^double x ^double y]
                                                    (/ (* (- x y)
                                                          (- x y)) y)) transposed-mtrx
-                                                 (fmap! (^:once fn* ^double [^double x] (/ x sum-obs))
+                                                 (fmap! (fn ^double [^double x]
+                                                          (/ x (neanderthal/sum (neanderthal-native/dv observed))))
                                                         (neanderthal/rank
                                                           (neanderthal/mv! transposed-mtrx one-v zero-v)
-                                                          (neanderthal/mv! mtrx one-v (neanderthal-native/dv ncols))))) one-v zero-v))))))
+                                                          (neanderthal/mv! mtrx one-v (neanderthal-native/dv ncols)))))
+                                          one-v zero-v))))))
 
-(neanderthal-native/dv (take 100000000 (range))) ;CAUSING OUT OF MEMORY SUBMIT PULL REQUEST
+
