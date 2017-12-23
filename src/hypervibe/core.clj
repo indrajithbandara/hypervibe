0dc67da9f4db9b632b7cb3c72afa1f698cedb2f8
ldev515 <gregory.adebesin@shopdirect.com>
2017-12-23 15:08:55 +0000

    deploy

--- src/hypervibe/core.clj
+++ src/hypervibe/core.clj
@@ -6,11 +6,11 @@
             [clojure.edn :as edn])
   (:import (java.io FileNotFoundException File IOException)
            (java.net URL)
-           (clojure.lang PersistentArrayMap PersistentVector)
+           (clojure.lang PersistentArrayMap PersistentVector LazySeq)
            (java.util Random Vector)))
 
-(def file-template (File. "template.edn"))
-(def file-template-packaged (File. "template-packaged.json"))
+(def file-hypervibe (File. "hypervibe.edn"))
+(def file-hypervibe-packaged (File. "hypervibe-packaged.json"))
 
 (defn- ^String rand16-char []
   (string/upper-case (Long/toHexString (Double/doubleToLongBits (.nextLong (Random.))))))
@@ -49,7 +49,8 @@
 
 (defn ^String edn->json
   [^File file]
-  (if-let [edn (slurp-edn file)]
+  (if-let
+    [edn (slurp-edn file)]
     (try (cheshire/generate-string edn
            {:pretty true})
          (catch Exception
@@ -67,55 +68,59 @@
 (def ^:const package-args "aws;cloudformation;package")
 
 (def ^:private ^String template-file-args
-  (if-let [json ^File (spit-json file-template file-template-packaged)]
-    (str "--template-file" ";" (.getAbsolutePath json))
+  (if-let
+    [json ^File (spit-json file-hypervibe
+                  file-hypervibe-packaged)]
+    (str "--template-file"
+      ";"
+      (.getAbsolutePath json))
     (throw (Exception. "Error spitting JSON to file"))))
 
-#_(defn package
-    [& {:keys [s3-bucket use-json? force-upload? kms-key-id]
-        :or {kms-key-id "" use-json? true force-upload? false}}]
-    (apply shell/sh
-      (remove nil?
-        ["aws"
-         "cloudformation"
-         "package"
-         "--template-file" (.getAbsolutePath (spit-json file-template file-template-packaged))
-         "--s3-bucket" s3-bucket
-         "--s3-prefix" "jars"
-         "--output-template-file" "template-packaged.json"
-         "--kms-key-id" kms-key-id
-         (if use-json? "--use-json")
-         (if force-upload? "--force-upload")])))
+(defn- ^LazySeq split-args
+  [^PersistentVector pv]
+  (mapcat #(string/split %
+             #";")
+    (remove nil?
+      pv)))
 
-
-(defn package
-  [& {:keys [s3-bucket use-json? force-upload? kms-key-id]
-      :or {kms-key-id "" use-json? true force-upload? false}}]
-  (if-let [json (spit-json file-template file-template-packaged)]
+(defn ^PersistentArrayMap package
+  [& {:keys [s3-bucket use-json? force-upload?
+             kms-key-id]
+      :or {use-json? true
+           force-upload? false}}]
+  (if-let
+    [json (spit-json file-hypervibe
+            file-hypervibe-packaged)]
     (apply shell/sh
-      (remove nil?
-        (mapcat #(string/split % #";")
-          ["aws;cloudformation;package"
-           (str "--template-file" ";" (.getAbsolutePath json))
-           (str "--s3-bucket" ";" s3-bucket)
-           "--s3-prefix;jars"
-           "--output-template-file;template-packaged.json"
-           (str "--kms-key-id" ";" kms-key-id)
-           (if use-json? "--use-json")
-           (if force-upload? "--force-upload")])))))
+      (split-args
+        ["aws;cloudformation;package"
+         (str "--template-file"
+           ";"
+           (.getAbsolutePath json))
+         (str "--s3-bucket"
+           ";"
+           s3-bucket)
+         "--s3-prefix;jars"
+         "--output-template-file;template-packaged.json"
+         (str "--kms-key-id"
+           ";"
+           kms-key-id)
+         (if use-json?
+           "--use-json")
+         (if force-upload?
+           "--force-upload")]))
+    (throw (Exception. "Failed to spit JSON"))))
 
 (defn deploy
   [& {:keys [capabilities stack-name no-execute-changeset?]
-      :or {capabilities "CAPABILITY_IAM" no-execute-changeset? false}}]
-  (apply
-    shell/sh
-    (remove nil?
-      ["aws"
-       "cloudformation"
-       "deploy"
-       "--template-file" "template-packaged.json"
-       "--stack-name" (str stack-name "-" (rand16-char))
-       "--capabilities" capabilities
+      :or {capabilities "CAPABILITY_IAM"
+           no-execute-changeset? false}}]
+  (apply shell/sh
+    (split-args
+      ["aws;cloudformation;deploy"
+       "--template-file;template-packaged.json"
+       (str "--stack-name" ";" (str stack-name "-" (rand16-char)))
+       (str "--capabilities" ";" capabilities)
        (if no-execute-changeset? "--no-execute-changeset")])))
 
 ;TODO
