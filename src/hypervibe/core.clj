94dade7531d0f9405125b41dbb26e8f53c97728a
ldev515 <gregory.adebesin@shopdirect.com>
2017-12-30 18:51:03 +0000

    tidy

--- src/hypervibe/core.clj
+++ src/hypervibe/core.clj
@@ -7,15 +7,16 @@
   (:import (java.io FileNotFoundException File IOException)
            (java.net URL)
            (clojure.lang PersistentVector LazySeq PersistentHashMap)
-           (java.util Random Vector)))
+           (java.util Random)))
 
-(def ^:private hyper-edn-file (File. "hypervibe.edn"))
-(def ^:private hyper-json-file (File. "hypervibe-packaged.json"))
+(def ^:const ^String hyper-edn "hypervibe.edn")
+(def ^:const ^String hyper-json ".hypervibe/hypervibe.edn")
+(def ^:const ^String hyper-pack-json ".hypervibe/hypervibe-packaged.json")
 
 (defn- ^String rand-16-char []
   (string/upper-case (Long/toHexString (Double/doubleToLongBits (.nextLong (Random.))))))
 
-(defn ^Boolean file-exi?
+(defn ^Boolean file-exist?
   [^File file]
   (.exists (.getAbsoluteFile file)))
 
@@ -27,19 +28,19 @@
   [^File file]
   (.endsWith (.getAbsolutePath file) ".json"))
 
-(defn ^Boolean json-file-exi?
+(defn ^Boolean json-file-exist?
   [^File file]
-  (and (file-exi? file)
+  (and (file-exist? file)
     (json-pref? file)))
 
-(defn ^Boolean edn-file-exi?
+(defn ^Boolean edn-file-exist?
   [^File file]
-  (and (file-exi? file)
+  (and (file-exist? file)
     (edn-pref? file)))
 
 (defn ^PersistentHashMap slurp-edn
   [^File file]
-  (if (edn-file-exi? file)
+  (if (edn-file-exist? file)
     (try (edn/read-string
            (slurp (.getAbsolutePath file)))
          (catch Exception _))))
@@ -52,31 +53,12 @@
          (catch Exception _))))
 
 (defn ^File spit-json
-  [^File edn ^File json]
-  (if (json-pref? json)
-    (try (do (spit (.getAbsolutePath json)
-               (edn->json edn))
-             (.getAbsoluteFile json))
+  []
+  (let [hyper-pack-dir (File. hyper-json)]
+    (try (do (spit (.getAbsolutePath hyper-pack-dir)
+               (edn->json (File. hyper-edn)))
+             (.getAbsoluteFile hyper-pack-dir))
          (catch IOException _))))
-
-(defn ^PersistentHashMap package
-  [& {:keys [s3-bucket use-json? force-upload?
-             kms-key-id]
-      :or {use-json? true
-           force-upload? false}}]
-  (if-let [json (spit-json hyper-edn-file hyper-json-file)]
-    (apply shell/sh
-      (remove nil?
-        ["aws"
-         "cloudformation"
-         "package"
-         "--template-file" (.getAbsolutePath json)
-         "--s3-bucket" s3-bucket
-         "--s3-prefix" "jars"
-         "--output-template-file" "template-packaged.json"
-         "--kms-key-id" kms-key-id
-         (if use-json? "--use-json")
-         (if force-upload? "--force-upload")]))))
 
 (defn- str-eq-kv
   [m]
@@ -91,9 +73,7 @@
   (if parameter-overrides (cons "--parameter-overrides" (str-eq-kv parameter-overrides))))
 
 (defn- stack-name-arg [stack-name]
-  (if stack-name-arg
-    (str stack-name "-" (rand-16-char))
-    "hypervibe"))
+  (if stack-name-arg (str stack-name "-" (rand-16-char)) "hypervibe"))
 
 (defn- capab-arg
   [capabilities]
@@ -106,31 +86,39 @@
   (into ["aws"
          "cloudformation"
          "deploy"
-         "--template-file" "template-packaged.json"
+         "--template-file" hyper-pack-json
          "--stack-name" (stack-name-arg stack-name)
          "--capabilities" (capab-arg capabilities)
          (if no-execute-changeset? "--no-execute-changeset")]
     (params-over-arg parameter-overrides)))
 
 (defn- ^PersistentVector pack-comm
-  [template-file s3-bucket use-json?
+  [s3-bucket use-json?
    force-upload? kms-key-id]
-  ["aws"
-   "cloudformation"
-   "package"
-   "--template-file" template-file
-   "--s3-bucket" s3-bucket
-   "--s3-prefix" "jars"
-   "--output-template-file" "template-packaged.json"
-   "--kms-key-id" kms-key-id
-   (if use-json? "--use-json")
-   (if force-upload? "--force-upload")])
+  (do (spit-json)
+      ["aws"
+       "cloudformation"
+       "package"
+       "--template-file" hyper-json
+       "--s3-bucket" s3-bucket
+       "--s3-prefix" "jars"
+       "--output-template-file" hyper-pack-json
+       "--kms-key-id" kms-key-id
+       (if (true? use-json?) "--use-json")
+       (if (true? force-upload?) "--force-upload")]))
+
+(defn ^PersistentHashMap package
+  [& {:keys [s3-bucket use-json? force-upload?
+             kms-key-id]}]
+  (apply-sh (pack-comm s3-bucket use-json? force-upload?
+              kms-key-id)))
 
 (defn ^PersistentHashMap deploy
   [& {:keys [stack-name capabilities no-execute-changeset?
              parameter-overrides]}]
   (apply-sh (dep-comm stack-name capabilities no-execute-changeset?
               parameter-overrides)))
+
 ;TODO
 ;(defn package)
 
