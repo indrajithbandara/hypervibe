7cd54425b5eedfc533f67c74540a43f2cbab2ddc
ldev515 <gregory.adebesin@shopdirect.com>
2018-01-13 18:17:34 +0000

    package

--- test/hypervibe/core_test.clj
+++ test/hypervibe/core_test.clj
@@ -3,8 +3,23 @@
             [hypervibe.core :as hyper]))
 
 (deftest test-target-dir
-  (testing "FIXME, I fail."
+  (testing
+    "Hypervibe artifact target directory"
     (is (= (:targ hyper/dirs)
           "target/"))))
+
+(deftest test-template-files
+  (testing "Hypervibe template file"
+    (is (= (:hyper hyper/files)
+          "hypervibe")
+      (= (:hyper-pack hyper/files)
+        "hypervibe-packaged"))))
+
+(deftest test-file-extensions
+  (testing "FIXME, I fail."
+    (is (and (= (:edn hyper/exten)
+               ".edn")
+          (= (:json hyper/exten)
+            ".json")))))
 
 
